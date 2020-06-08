a9b7362e3b5a8259d5832f388405e28d00f26277
yeicobF <A291423@alumnos.uaslp.mx>
2020-06-08 15:59:35 -0500

    Prueba para ver cómo subir cambios
    Prueba.

--- Archivo.java
+++ Archivo.java
@@ -6,8 +6,8 @@ import java.io.FileNotFoundException;
  * Esta clase manejará los archivos necesarios:
  *  Leer archivos (Créditos, instruciones, etcétera)
  *  Crear archivos (puntuaciones, etcétera)
- * 
- * @author (Team Naves) 
+ *
+ * @author (Team Naves)
  * @version (Viernes, 29 de mayo de 2020)
  */
 public class Archivo extends Actor
@@ -29,7 +29,7 @@ public class Archivo extends Actor
     public void act(){
         abrirMostrarCerrarArchivo();
     }
-    
+
     /* Método que hará los 3 métodos mencionados sin tener que llamarlos manualmente cada uno.*/
     public void abrirMostrarCerrarArchivo(){
         abrirArchivo();
@@ -50,7 +50,7 @@ public class Archivo extends Actor
         boolean primerLinea = true; //En la primer linea del texto indicaré el tamaño del archivo, es decir, sus filas para así centrarlo.
         /*numLineas indicará el total para reescalar y se irá restando para ir bajando en la pantalla.
            altura indica la altura actual del texto. Esta irá bajando dependiendo del número de línea actual.*/
-        int numLineas = 0, altura = 0; 
+        int numLineas = 0, altura = 0;
         Etiqueta e = new Etiqueta(30, Color.WHITE, null, null);
         //public Etiqueta(int tamañoFuente, Color colorFuente, Color colorFondo, Color bordeFuente)
         while(archivo.hasNextLine() && !textoLeido){
@@ -67,13 +67,13 @@ public class Archivo extends Actor
                 altura = w.getHeight()/2-40 + numLineas * 30; //La altura de cada línea de texto será de 30
             else//Si se encuentra debajo de la mitad, ir disminuyendo.
                 altura = w.getHeight()/2+40 - numLineas * 30;
-            
+
             numLineas --;//Disminuir el número de línea actual.
             /*Encontré una mejor manera para crear los cuadros de texto sin tener que convertirlos en botones.
                Esto será para lo que no necesite botones. Para esto al final del ciclo, se crearán los botones.*/
-            
+
             //Etiqueta e = new Etiqueta(archivo.nextLine(), 30, Color.WHITE, null);
-            
+
             //public GreenfootImage crearCuadroTexto(String s)
             //Se crea el cuadro de texto en el objeto Etiqueta. Se hace su setImage.
             e.crearCuadroTexto(archivo.nextLine());
