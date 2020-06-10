271889a9924a145a2b4ead5a4db6ffdb520c23b4
yeicobF <A291423@alumnos.uaslp.mx>
2020-06-10 12:12:03 -0500

    Al pasar el mouse encima de un botón, crece
    - Ahora al pasar el mouse encima de los botones, estos crecen para que se vea más animado.
    - Había un problema en el que la imagen grande siempre estaba establecida, por lo que tuve que utilizar una imagen auxiliar para esto. Desafortunadamente esto tampoco no servía.
    - Luego me di cuenta de que cuando modificaba la imagen grande y la auxiliar yambién se modificaba la original porque de alguna forma estaban ligadas como con los apuntadores porque la asigniación fue directa desde la imagen original, así que hice ese proceso por separado asignando las mismas imágenes pero repitiendo el proceso y funcionó.
    - Ahora el problema está en que los botones de imágenes como el de pausa no funcionan así porque no puedo asignar de manera dierente, ya que sólorecibo la imagen. Lo que estoy pensando para solucionar esto es que el constructor solo reciba el nombre de la imagen en lugar de la imagen y se haga la asignación de uno por uno ahí mismo.

--- Boton.java
+++ Boton.java
@@ -9,19 +9,48 @@ import greenfoot.*;
 public class Boton extends Actor
 {
     // instance variables - replace the example below with your own
+    GreenfootImage imagenOriginal; //Guardará la imagen original para no reescalarla y que no sepierda la calidad.
+    GreenfootImage imagenGrande; //Guardará la imagen reescalada para cuando pase el mouse por encima.
+    GreenfootImage imagenAuxiliar; //Cuando se use imagenGrande y se modifique a partir de la original, se necesita auxiliar para que la original no cambie.
     String texto; //El texto del botón
     private Etiqueta e;
     /*Constructor para cuando el botón creado es una imagen.*/
     private Boton(GreenfootImage imagen){
         setImage(imagen); //Establece la imagen del botón.
+        imagenOriginal =  imagenAuxiliar = imagen;
+        imagenGrande = Imagen.modificarEscalaImagen(imagenAuxiliar, 4, 5);
     }
     //public Etiqueta(int tamañoFuente, Color colorFuente, Color colorFondo, Color bordeFuente)
     private Boton(String texto, int tamañoFuente, Color colorFuente, Color colorFondo, Color bordeFuente){
         this.texto = texto;
         e = new Etiqueta(tamañoFuente, colorFuente, colorFondo, bordeFuente);
-        setImage(e.crearCuadroTexto(texto)); //Crear el cuadro de texto y hacerlo como la imagen de aquí.
+        imagenOriginal = e.crearCuadroTexto(texto);
+        
+        setImage(imagenOriginal); //Crear el cuadro de texto y hacerlo como la imagen de aquí.
         //No necesitamos el método crearBoton. Aquí ya se crea con la imagen del texto necesario.
+        //imagenAuxiliar = getImage();
+        //Cambiar el tamaño del fondo de la imagen que cambiará al pasa el mouse por encima. Esto si tenía fondo anteriormente.
+        if(colorFondo != null) //GreenfootImage(texto, tamañoFuente, colorFuente, colorFondo, bordeFuente)
+            imagenAuxiliar = imagenGrande =  new GreenfootImage(texto, tamañoFuente, Color.BLACK, Color.WHITE, bordeFuente);
+        else
+            /* Tuve que hacer esto de asignar la imagenAuxiliar a partir de crear tel texto, ya que si no
+            la imagenAuxiliar tiene una especie de conexión como con los apuntadores y cada que se modifica
+            la imagen grande o auxiliar también se modifica la imagenOriginal.
+            De esta forma ya trabajan de forma independiente.*/
+            imagenAuxiliar = e.crearCuadroTexto(texto);
+        /*En la instrucción de abajo hay un problema, se modifica la imagen pero se pone esa en lugar de la original.*/
+        imagenGrande = Imagen.modificarEscalaImagen(imagenAuxiliar, 4, 5);
+        //setImage(imagenOriginal);
     }
+    
+    public void act(){
+        if (Greenfoot.mouseMoved(this)) //Si el mouse se pasa por encima.
+            setImage(imagenGrande);    
+            // setImage(Imagen.modificarEscalaImagen(imagenAuxiliar, 4, 5));
+        if (Greenfoot.mouseMoved(null) && !Greenfoot.mouseMoved(this)) //Si el mouse no se pasa por encima.
+            setImage(imagenOriginal);
+    }
+    
     public static Boton creaBoton(World mundoActual, String texto, int x, int y,
                     Color colorFuente, Color colorFondo, Color bordeFuente, int tamañoFuente)
     {
