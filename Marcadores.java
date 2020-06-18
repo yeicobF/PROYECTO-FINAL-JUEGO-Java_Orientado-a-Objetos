305dd6fe3077f32fcab5eedaef3a77cac70b3d27
yeicobF <A291423@alumnos.uaslp.mx>
2020-06-18 17:56:55 -0500

    Guardado de nombre CASI CORRECTO
    Ya es funcional. Solo que hay dos problemas:
    - Al ingresar el nombre muestra las opciones de "Aceptar" y "Cancelar". La opción de Cancelar evidentemente no la queremos, por la que utilizaré otro método en el que podemos establecer nuestras condiciones. Este será: showInputDialog​(Component parentComponent, Object message, String title, int messageType, Icon icon, Object[] selectionValues, Object initialSelectionValue). A ver si así funciona, ya que si le dan a cancelar el programa se detiene.
    - Revisar que no hayan marcadores repetidos cn el mismo nombre. Para esto revisaremos antes el archivo, pasaremos los datos a un arrayList y ahí se hará la búsqueda de algún dato igual.

--- Marcadores.java
+++ Marcadores.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import javax.swing.JOptionPane; //Para el pop-up que pedirá el nombre del jugador. Que tenga un límite de caracteres.
+import java.awt.HeadlessException; //Excepción de input de JOption.
 /**
  * Clase en la que se manejarán los marcadores. Se agregarán las nuevas puntuaciones
  *  y se mostrarán las que ya hay. Se podrían mostrar hasta un límite de n puntuaciones.
@@ -24,28 +25,52 @@ public class Marcadores
         fechaActual = fecha.getFecha();
         pedirNombreUsuario();
     }
+    public static void mostrarMarcadores(){
+        //Abrir el archivo .txt con los marcadores y mostrar cierto número de puntuaciones.
+    }
     //Método que pedirá el nombre de usuario del jugador que será añadido a los marcadores.
         /* Fuentes: 
          - https://www.greenfoot.org/topics/3583
          - https://mkyong.com/swing/java-swing-how-to-make-a-confirmation-dialog/
          */
-    private void pedirNombreUsuario(){
-        int confirmacion, maxCaracteres = 10;
-        nombreJugador = JOptionPane.showInputDialog("Ingresa tu nombre (Máximo de caracteres = "+ maxCaracteres +"):");
-        if(nombreJugador.length() > maxCaracteres){ //Máximo de caracteres = 10.
-            //Volver a llamar al método, pero primero mostrar otro cuadro de texto diciendo que pasó el límite.
-            confirmacion = JOptionPane.showConfirmDialog(null, 
-                                            "Superaste el número de caracteres ("+ maxCaracteres +").\nVuelve a ingresar tu nombre.", 
-                                                     "ERROR EN NOMBRE JUGADOR", 
-                                                     JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
-            pedirNombreUsuario(); //Volver a pedir el nombre.                                         
+    private void pedirNombreUsuario(){  
+        int confirmacion = 1, maxCaracteres = 10;
+        while(confirmacion == 1){// 0 = Sí está seguro del nombre, 1 = No está seguro del nombre.
+            try{/*public static String showInputDialog​(Component parentComponent, Object message,
+                                                        String title, int messageType) throws HeadlessException*/
+                nombreJugador = JOptionPane.showInputDialog(null, "Ingresa tu nombre (Máximo de caracteres = "+ maxCaracteres +"):",
+                                                        "NOMBRE", JOptionPane.PLAIN_MESSAGE);
+                }catch(HeadlessException he){
+                    //https://docs.oracle.com/en/java/javase/11/docs/api/java.desktop/java/awt/GraphicsEnvironment.html#isHeadless()
+                    //true if this environment cannot support a display, keyboard, and mouse
+                    System.out.println("El entorno no soporta un monitor, teclado, y mouse.");
+                }
+            if(nombreJugador.length() > maxCaracteres){ //Máximo de caracteres = 10.
+                //Volver a llamar al método, pero primero mostrar otro cuadro de texto diciendo que pasó el límite.
+                confirmacion = JOptionPane.showConfirmDialog(null, 
+                                                "Superaste el número de caracteres ("+ maxCaracteres +").\nVuelve a ingresar tu nombre.", 
+                                                         "ERROR EN NOMBRE JUGADOR", 
+                                                         JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);                                    
+            }
+            else //No se superó el número de caracteres.
+                //Mostrar cuadro de confirmación con SÍ y NO como opciones.
+                confirmacion = JOptionPane.showConfirmDialog(null, "¿Estás seguro del nombre?", 
+                                                             "CONFIRMACIÓN NOMBRE JUGADOR", 
+                                                             JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE); // 0=yes, 1=no
         }
-        //Mostrar cuadro de confirmación con SÍ y NO como opciones.
-        confirmacion = JOptionPane.showConfirmDialog(null, "¿Estás seguro del nombre?", 
-                                                     "CONFIRMACIÓN NOMBRE JUGADOR", 
-                                                     JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE); // 0=yes, 1=no
-        if(confirmacion == 1)
-            pedirNombreUsuario(); //Volver a pedir el nombre de usuario.
+        // //static void showInternalMessageDialog​(Component parentComponent, Object message, String title, int messageType)
+        // JOptionPane.showConfirmDialog(null, "Nombre guardado con éxito.", "ÉXITO", JOptionPane.INFORMATION_MESSAGE);
+        
+        try{/*//public static void showMessageDialog​(Component parentComponent, Object message,
+                                                      String title, int messageType) throws HeadlessException*/
+            JOptionPane.showMessageDialog(null, "Nombre guardado con éxito.", "ÉXITO", JOptionPane.INFORMATION_MESSAGE);
+            }catch(HeadlessException he){
+                //https://docs.oracle.com/en/java/javase/11/docs/api/java.desktop/java/awt/GraphicsEnvironment.html#isHeadless()
+                //true if this environment cannot support a display, keyboard, and mouse
+                System.out.println("El entorno no soporta un monitor, teclado, y mouse.");
+            }
+             // if(confirmacion == 1)
+            // pedirNombreUsuario(); //Volver a pedir el nombre de usuario.
         //Establecer un máximo de caracteres para que no se ingresen demasiados y saturen la pantalla.
         
     }
