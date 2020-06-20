306c0d9a4118c8ca30cff3cc2766a81908951524
yeicobF <A291423@alumnos.uaslp.mx>
2020-06-20 18:46:46 -0500

    INTRODUCCIÓN DE NIVELES FUNCIONAL
    Ahora al inicio de cada nivel se muestra una historia. Se presionará enter para cambiar de imagen.
    - Intentaré optimizar los métodos para que quede más limpio el código.

--- GameOver.java
+++ GameOver.java
@@ -1,10 +1,10 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import greenfoot.World;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.Greenfoot;
 /**
  * Write a description of class GameOver here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Daniel) 
+ * @version (Viernes, 19 de junio de 2020)
  */
 public class GameOver extends World
 {
@@ -17,11 +17,11 @@ public class GameOver extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 600, 1); 
+        setBackground("escenarios/gameOver.png");
     }
     public void act(){
-    if(Greenfoot.isKeyDown("enter"))
-    {
-     Greenfoot.setWorld(new Menu());   
-    }
+        if(Greenfoot.isKeyDown("enter")){
+            Greenfoot.setWorld(new Menu());   
+        }
     }
 }
--- Intro.java
+++ Intro.java
@@ -1,10 +1,11 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import greenfoot.World;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.Greenfoot;
 /**
- * Write a description of class Intro here.
+ * Clase en la que se manejará la introducción de cada nivel.
+ * Para llamar la introduccion 1,2 o 3 usar: Greenfoot.setWorld(new Intro("inserta aca el numero"));
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Daniel) 
+ * @version (Viernes, 19 de junio de 2020)
  */
 public class Intro extends World
 {
@@ -17,74 +18,64 @@ public class Intro extends World
      * Constructor for objects of class Intro.
      * 
      */
-    public Intro(int tipo)
-    {    
+    public Intro(int nivelIntro){    
         // Create a new world with 1000x600 cells with a cell size of 1x1 pixels.
         super(1000, 600, 1);
-        actualizarvariables(tipo);
-
+        nivel = nivelIntro; //El número de la introducción indicará el número del nivel.
+        actualizarvariables(nivelIntro);
     }
     public void act(){
-        switch(this.contador)
-        {
+        switch(this.contador){
            case 1: //Aqui se ejecuta la introduccion.
-            if(Greenfoot.isKeyDown("enter"))//Checa si se presiono alguna 
-             {
+               if(Greenfoot.isKeyDown("enter"))//Checa si se presiono alguna
                     this.contador=2;//Contador con el que se sabe cuando se dio un click y modifica el objeto de texto
-             }
-             break;
+               break;
            case 2: 
-           if(Greenfoot.isKeyDown("enter"))//Checa si se presiono alguna tecla
-             {
+               if(Greenfoot.isKeyDown("enter"))//Checa si se presiono alguna tecla
                     this.contador=9;//Contador con el que se sabe cuando se dio un click y modifica el objeto de texto
-             }
-           break;
+               break;
            case 3:
-           if(Greenfoot.isKeyDown("enter"))//Checa si se presiono alguna tecla
-             {
+               if(Greenfoot.isKeyDown("enter"))//Checa si se presiono alguna tecla
                     this.contador=14;//Contador con el que se sabe cuando se dio un click y modifica el objeto de texto
-             }
-           default: break;  
+               default: break;  
         }
             
         //}while(this.contador<=7);
-        if(contador!=Bandera)
-         {
-          Bandera++;
-          Greenfoot.setWorld(new Niveles(nivel));//Al terminar ya llama al juego.
+        if(contador!=Bandera){
+              Bandera++;
+              Greenfoot.setWorld(new Niveles(nivel));//Al terminar ya llama al juego.
          }
     }
     
-    public void actualizarvariables(int tipo)
-    {
-       switch(tipo)
-       {
+    public void actualizarvariables(int tipo){
+       switch(tipo){
          case 1: //Aqui se toma como el inicio del juego
-          this.contador=1;
-          this.nivel=1;
-          this.Bandera=1;
-          Texto Introduccion = new Texto(0); // Objeto permanente para que le jugador sepa que debe presionar Enter para avanzar dialogos
-          Texto Cuadro = new Texto(1); //Objeto reutilizable para los dialogos
-          addObject(Introduccion,500,560);//Coloca la instruccion
-          addObject(Cuadro,500,200);//Coloca el cuadro de texto
-          break;
+              this.contador=1;
+              this.nivel=1;
+              this.Bandera=1;
+              Texto Introduccion = new Texto(0); // Objeto permanente para que le jugador sepa que debe presionar Enter para avanzar dialogos
+              Texto Cuadro = new Texto(1); //Objeto reutilizable para los dialogos
+              addObject(Introduccion,500,560);//Coloca la instruccion
+              addObject(Cuadro,500,200);//Coloca el cuadro de texto
+              break;
          case 2://Aqui se toma como el 
-          this.contador=8;
-          this.nivel=2;
-          this.Bandera=8;
-          Texto Introduccion2 = new Texto(0); // Objeto permanente para que le jugador sepa que debe presionar Enter para avanzar dialogos
-          Texto Cuadro2 = new Texto(8); //Objeto reutilizable para los dialogos
-          addObject(Introduccion2,500,560);//Coloca la instruccion
-          addObject(Cuadro2,500,200);//Coloca el cuadro de texto
-          break;
-          case 3:
-          this.contador=8;
-          this.nivel=3;
-          this.Bandera=8;
-          Texto Introduccion3 = new Texto(0); // Objeto permanente para que le jugador sepa que debe presionar Enter para avanzar dialogos
-          Texto Cuadro3 = new Texto(14); //Objeto reutilizable para los dialogos
-          addObject(Introduccion3,500,560);//Coloca la instruccion
-          addObject(Cuadro3,500,200);//Coloca el cuadro de texto
+              this.contador=8;
+              this.nivel=2;
+              this.Bandera=8;
+              Texto Introduccion2 = new Texto(0); // Objeto permanente para que le jugador sepa que debe presionar Enter para avanzar dialogos
+              Texto Cuadro2 = new Texto(8); //Objeto reutilizable para los dialogos
+              addObject(Introduccion2,500,560);//Coloca la instruccion
+              addObject(Cuadro2,500,200);//Coloca el cuadro de texto
+              break;
+         case 3:
+              this.contador=8;
+              this.nivel=3;
+              this.Bandera=8;
+              Texto Introduccion3 = new Texto(0); // Objeto permanente para que le jugador sepa que debe presionar Enter para avanzar dialogos
+              Texto Cuadro3 = new Texto(14); //Objeto reutilizable para los dialogos
+              addObject(Introduccion3,500,560);//Coloca la instruccion
+              addObject(Cuadro3,500,200);//Coloca el cuadro de texto
+              break;
          default: break;
        }
     }
