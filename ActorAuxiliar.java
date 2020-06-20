5b89b7aafac86a6c551f0c11f945c7e0a9d671e3
yeicobF <60598673+yeicobF@users.noreply.github.com>
2020-06-20 14:42:57 -0500

    Cambié detalles de AutorAuxiliar
    Solamente el nombre de autor y fecha.

--- ActorAuxiliar.java
+++ ActorAuxiliar.java
@@ -4,8 +4,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * Clase que será un auxiliar para poder manejar cosas que no tienen un uso general o específico.
  *  Por ejemplo para guardar cuadros de texto y obtener sus medidas.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Jacob) 
+ * @version (Viernes, 19 de junio de 2020)
  */
 public class ActorAuxiliar extends Actor
 {
