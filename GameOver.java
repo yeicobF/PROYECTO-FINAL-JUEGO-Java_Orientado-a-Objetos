0f21a9230882eb7b7cb7a0f53cecb8da62aa74b0
yeicobF <A291423@alumnos.uaslp.mx>
2020-06-21 02:39:31 -0500

    Agregué una nueva imagen de GameOver
    Esta nueva imagen no muestra el texto de presiona Enter...

--- GameOver.java
+++ GameOver.java
@@ -19,7 +19,7 @@ public class GameOver extends World
     {    /* Se recibe la nave que controlamos para recibir sus estadísticas. De esta manera se podrá llamar a jugador desde aquí.*/
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 600, 1); //Para que no se cree botón de "siguiente". Solo el de regresar al menú. 
-        setBackground("escenarios/gameOver.png");
+        setBackground("escenarios/gameOver.jpg");
         this.nave = nave;
     }
     public void act(){
