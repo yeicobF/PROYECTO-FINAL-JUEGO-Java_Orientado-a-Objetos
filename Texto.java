916571d704e7f012599ba804f9d1fbec3cd11379
yeicobF <A291423@alumnos.uaslp.mx>
2020-06-20 20:06:00 -0500

    Mejoré la implementación de Intro y Texto
    Reduje el código necesario de las clases. De esta manera se hace menos enrevesado y más limpio. Aún no borro lo que no se usa. Ahora limpiaré el código.

--- Texto.java
+++ Texto.java
@@ -1,10 +1,10 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import greenfoot.Actor;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.Greenfoot;
 /**
  * Clase que mostrará las imágenes de cada introducción de nivel.
  * La imagen se mostrará dependiendo del nivel y el número de imagen.
  * 
- * @author (Daniel) 
+ * @author (Daniel, Jacob (optimización)) 
  * @version (Sábado, 20 de junio de 2020)
  */
 public class Texto extends Actor
@@ -72,8 +72,12 @@ public class Texto extends Actor
     }    
     /** Veriica si se presoonó enter para cambiar de imagen.*/
     private boolean isEnter(){
-        if(Greenfoot.isKeyDown("enter"))
-            return true;
-        return false;
+        // if(Greenfoot.isKeyDown("enter"))
+            // return true;
+        return Greenfoot.isKeyDown("enter"); //Devuele lo que salga dr aquí.
+    }
+    /** Método que verifica si se terminó la intro, para así proceder a avanzar de nivel.*/
+    public boolean isIntroFinal(){
+        return numImagen >= limiteImagen + 1; //Si se superó el número de imagen, se devolverá true.
     }
 }
