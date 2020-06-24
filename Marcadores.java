3bcf91c939d526d0dd1983c013bd6938ba033691
yeicobF <A291423@alumnos.uaslp.mx>
2020-06-24 17:33:20 -0500

    Marcadores LIMPIA

--- Marcadores.java
+++ Marcadores.java
@@ -1,8 +1,6 @@
-import greenfoot.Actor;
 import greenfoot.World;
 import greenfoot.Color;
 import greenfoot.GreenfootImage;
-import greenfoot.Greenfoot;
 import java.util.Scanner;
 /**
  * Clase en la que se manejarán los marcadores. Se agregarán las nuevas puntuaciones
@@ -24,13 +22,11 @@ public class Marcadores extends Menu
     private Scanner archivo; //Para recorrer el archivo y mostrarlo.
     private ActorAuxiliar[] textos; //Cuadros de texto como actor para colocarlos con facilidad.
     private int[] xTextos; //Guardará las coordenadas de los textos.
-    // private int sumaTamTextos; //La suma del largo de los textos para acomodarlos en pantalla.
-    private int espacioEntreTextos; //El espacio que habrá entre texto y texto.
     /* 0 - Nombre
        1 - Puntos
        2 - Nivel
        3 - Fecha*/
-       //Para solo mostrarlos.
+    private int espacioEntreTextos; //El espacio que habrá entre texto y texto.
     /** Constructor de Marcadores que recibe un booleano que verifica
        si se quieren mostrar los marcadores en este constructor o no.
        Esto  servirá para que no se muestren los marcadores al ser llamado
@@ -41,8 +37,7 @@ public class Marcadores extends Menu
         super(false); //Construir la pantalla de marcadores.
         //public Etiqueta(int tamañoFuente, Color colorFuente, Color colorFondo, Color bordeFuente)
         texto = new Etiqueta(40, Color.WHITE, null, null);
-        xTextos = new int[4];
-        //https://stackoverflow.com/questions/5364278/creating-an-array-of-objects-in-java
+        xTextos = new int[4];//Fuente: https://stackoverflow.com/questions/5364278/creating-an-array-of-objects-in-java
         textos = new ActorAuxiliar[] {new ActorAuxiliar(), new ActorAuxiliar(), new ActorAuxiliar(), new ActorAuxiliar()}; //Crear el arreglo de los textos.
         //Darles sus respectivas imágenes.
         textos[0].setImage(texto.crearCuadroTexto("NOMBRE"));
@@ -55,14 +50,11 @@ public class Marcadores extends Menu
             mostrarMarcadores();
     }
     public Marcadores(Jugador jugador){
-        // super(1000, 600, 1); //Construir la pantalla de marcadores.
-        // setBackground("images/espacio5.jpg");
         this(false); //Llamar al otro constructor de marcadores.
         this.jugador = jugador; //Se recibe el objeto con su información para agregarla a los marcadores.
-        
         agregaJugadorMarcadores();
-        //public Archivo(String nombreArchivo, String nombreArchivoEscribir)
-            //Se ordena el archivo de marcadores con el nuevo que ingresamos.
+        /*public Archivo(String nombreArchivo, String nombreArchivoEscribir)
+            -> Se ordena el archivo de marcadores con el nuevo que ingresamos.*/
         marcadores = new Archivo("archivos/marcadoresSinOrdenar.txt", "archivos/marcadores.txt");
         mostrarMarcadores();
     }
@@ -70,13 +62,13 @@ public class Marcadores extends Menu
     public void act(){
         volverMenu();
     }
-    
     /** Método que agregará al jugador al archivo de los marcadores.*/
     private void agregaJugadorMarcadores(){
         Archivo a = new Archivo("archivos/marcadoresSinOrdenar.txt");
         /* Escribir al final del archivo de los marcadores el jugador que se ha recibido.*/
         a.escribirAlFinalArchivo(jugador.getInformacion());
     }
+    /** Método para mostrar los marcadores en orden descendiente.*/
     public void mostrarMarcadores(){
         //Abrir el archivo .txt con los marcadores y mostrar cierto número de puntuaciones.
         //public Archivo(String nombreArchivo, int tamañoFuente, Color colorFuente)
@@ -86,8 +78,7 @@ public class Marcadores extends Menu
         fondo = getBackground();
         marcadores = new Archivo("archivos/marcadores.txt", 30, Color.WHITE);
         archivo = marcadores.getArchivo();
-        // marcadores.mostrarArchivo(this);
-        while(archivo.hasNext() && numeroMarcador <= maxMarcadores){ //Mientras
+        while(archivo.hasNext() && numeroMarcador <= maxMarcadores){ //Mientras el archivo tenga más texto.
             fondo.drawImage(texto.crearCuadroTexto(archivo.next()), xTextos[0] - texto.getXCentrada(), altura); //Nombre
             fondo.drawImage(texto.crearCuadroTexto(archivo.next()), xTextos[1] - texto.getXCentrada(), altura); //Puntos
             fondo.drawImage(texto.crearCuadroTexto(archivo.next()), xTextos[2] - texto.getXCentrada(), altura); //Nivel
@@ -98,16 +89,15 @@ public class Marcadores extends Menu
         archivo.close(); //Cerrar archivo.
         marcadores.cerrarArchivo(); //Para asegurar.
     }
-    /*Método que calcula el espacio entre cuadro de texto y cuadro de texto para que queden bien repartidos.*/
+    /** Método que calcula el espacio entre cuadro de texto y cuadro de texto para que queden bien repartidos.*/
     private void calculaEspacioEntreTextos(){
         int sumaTamTextos; //Es auxiliar nada más.
         sumaTamTextos = espacioEntreTextos = 0;
         for(int i = 0; i < 4; i++)
             sumaTamTextos += textos[i].getImage().getWidth(); //El getWidth devuelve el ancho desde la mitad de la imagen.
         espacioEntreTextos = (getWidth() - sumaTamTextos)/5; //Calcular el espacio entre cada texto dividiendo lo sobrante de la pantalla entre 5.
-        //System.out.println("SumaTamTextos = "+ sumaTamTextos +", Espacio entre textos: "+ espacioEntreTextos +", getWidth(): "+ getWidth());
     }
-    /* Método que agregará los cuadros de texto de acuerdo al espacio entre estos.*/
+    /** Método que agregará los cuadros de texto de acuerdo al espacio entre estos.*/
     private void agregaCuadrosTexto(){
         int xActual = espacioEntreTextos; //Auxiliar para ver en dónde va la x para colocar los cuadros.
         for(int i = 0; i < 4; i++){
@@ -117,11 +107,4 @@ public class Marcadores extends Menu
             xActual += espacioEntreTextos + textos[i].getImage().getWidth();
         }
     }
-    
-    // private class arrayListMarcadores{
-        // private ArrayList<Marcadores> listaMarcadores; //ArrayList de los marcadores.
-        // public void addMarcador(){ //Método en donde agregaremos las puntuaciones.
-            
-        // }
-    // }
 }
