a52812721439ac8130fc0b4d698eda11c32275f5
yeicobF <A291423@alumnos.uaslp.mx>
2020-06-24 17:23:23 -0500

    Intro LIMPIA

--- Intro.java
+++ Intro.java
@@ -4,20 +4,17 @@ import greenfoot.GreenfootImage;
 import greenfoot.GreenfootSound; //Para el sonido de la intro.
 /**
  * Clase en la que se manejará la introducción de cada nivel.
- * Para llamar la introduccion 1,2 o 3 usar: Greenfoot.setWorld(new Intro("inserta aca el numero"));
+ * Para llamar la introduccion 1, 2 o 3 usar: Greenfoot.setWorld(new Intro("inserta aca el numero"));
  * 
- * @author (Daniel, Jacob (optimización)) 
+ * @author (Team Naves) 
  * @version (Sábado, 20 de junio de 2020)
  */
 public class Intro extends World
 {
-    private GreenfootSound musica; 
-    private int nivel;//Contador para saber que nivel es el que se debe llamar de forma automatica.
+    private GreenfootSound musica;
     private Texto textosIntro;//Esta variable sera el objeto donde se almacena la imagen con el texto
-    /**
-     * Constructor for objects of class Intro.
-     * 
-     */
+    private int nivel;//Contador para saber que nivel es el que se debe llamar de forma automatica.
+    /** Constructor de la clase Intro que recibe el nivel al que se pasó.*/
     public Intro(int nivelIntro){
         super(1000, 600, 1);
         musica = new GreenfootSound("historia.mp3");
