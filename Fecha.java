0a72d1b262c453c4615e8840e941b6c360b737f1
yeicobF <A291423@alumnos.uaslp.mx>
2020-06-24 19:49:54 -0500

    Fecha LIMPIA

--- Fecha.java
+++ Fecha.java
@@ -6,18 +6,18 @@ import java.time.LocalDate;
  * Obtuve la forma de obtener la fecha con nuestro formato indicado en: 
  *           https://www.javatpoint.com/java-get-current-date
  * 
- * @author (Team Naves) 
+ * @author (Jacob) 
  * @version (Miércoles, 17 de junio de 2020)
  */
-public abstract class Fecha{
+public abstract class Fecha
+{
     //Aquí le daremos el formato a la fecha. Día/Mes/Año Hora:Minutos:segundos.
     private static DateTimeFormatter formatoFecha  = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
     //Aquí se obtendrá la fecha local actual.
     private static LocalDateTime fechaActual;
-    /*Método para obtener la fecha actual en el formato dado arriba.*/
+    /** Método para obtener la fecha actual en el formato dado arriba.*/
     public static String getFecha(){
        fechaActual = LocalDateTime.now(); //Obtener la fecha actual.
-       //System.out.println(formatoFecha.format(fechaActual));  
        return formatoFecha.format(fechaActual); //Regresar la fecha actual con el formato que le dimos.
     }
    
